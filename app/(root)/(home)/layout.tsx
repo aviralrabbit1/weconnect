414190373c4af219832c85484f4a4ec664c1f3be
aviralrabbit1 <aviral.robin@gmail.com>
2025-08-21 16:32:20 +0530

    stashing

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,13 +5,14 @@ import React, { ReactNode } from 'react'
 const Homelayout = ({ children }: {children: ReactNode}) => {
   return (
     <main className='relative'>
+      <Navbar/>
         <div className="flex">
             <Sidebar/>
-                <section className="flex flex-1 flex-col min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
-                    <div className="w-full">
-                        {children}
-                    </div>                    
-                </section>
+              <section className="flex flex-1 flex-col min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+                  <div className="w-full">
+                      {children}
+                  </div>                    
+              </section>
         </div>
     </main>
   )
